f7629d5235f47238f262a7b5db3595d534371885
Alexander Shadrin <sashashadrin@gmail.com>
2017-03-17 14:58:11 +0300

    handle only text msg

--- dotaBot.js
+++ dotaBot.js
@@ -7,17 +7,19 @@ class DotaBot extends BotEngine {
 
   handleUpdates(update) {
     const msgText = update.message.text;
-    msgText.split(' ').map( x=> {
-      if ( x.toLowerCase().substring(0, x.length -1) === 'дот' || x.toLowerCase() === 'dota' ) {
-        this.sendMessage({
-          chat_id: update.message.chat.id,
-          parse_mode: 'HTML',
-          text: ` 
-          <b>DOTA ALERT</b>
-          `,
-        });
-      }
-    })    
+    if (update.message.text) {
+      msgText.split(' ').map( x=> {
+        if ( x.toLowerCase().substring(0, x.length -1) === 'дот' || x.toLowerCase() === 'dota' ) {
+          this.sendMessage({
+            chat_id: update.message.chat.id,
+            parse_mode: 'HTML',
+            text: ` 
+            <b>DOTA ALERT</b>
+            `,
+          });
+        }
+      })
+    }
   }
 }
 
