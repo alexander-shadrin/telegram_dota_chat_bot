efdecc30ce08eed21acc0f313afe13b1532dbccb
Alexander Shadrin <sashashadrin@gmail.com>
2017-03-20 16:04:33 +0300

    add echo command

--- dotaBot.js
+++ dotaBot.js
@@ -7,19 +7,41 @@ class DotaBot extends BotEngine {
 
   handleUpdates(update) {
     const msgText = update.message.text;
+    const chatId = update.message.chat.id;
     if (update.message.text) {
-      msgText.split(' ').map( x=> {
-        if ( x.toLowerCase().substring(0, 3) === 'дот' || x.toLowerCase() === 'dota' ) {
-          this.sendMessage({
-            chat_id: update.message.chat.id,
-            parse_mode: 'HTML',
-            text: ` 
-            <b>DOTA ALERT</b>
-            `,
-          });
-        }
-      })
+      if (msgText[0] === '/') {
+        this.executeCommand(chatId, msgText);
+      } else {
+        msgText.split(' ').map( x=> {
+          if ( x.toLowerCase().substring(0, 3) === 'дот' || x.toLowerCase() === 'dota' ) {
+            this.sendMessage({
+              chat_id: chatId,
+              parse_mode: 'HTML',
+              text: ` 
+              <b>DOTA ALERT</b>
+              `,
+            });
+          }
+        })
+      }
     }
+  }
+
+  executeCommand(chatId, cmdText) {
+    let commandParams = cmdText.split(' ');
+    const commandType = commandParams.shift().substr(1);
+    if (this[commandType]) {
+      const responce = this[commandType](commandParams);
+      this.sendMessage({
+        chat_id: chatId,
+        parse_mode: responce.parse_mode || 'HTML',
+        text: responce.text,
+      });
+    }
+  }
+
+  echo(params) {
+    return { text: params.join(' ') };
   }
 }
 
